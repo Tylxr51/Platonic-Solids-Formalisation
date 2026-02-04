1c4c03405d963fc5da7162d7061799da9cf508c5
Cadelliot <elliotmc_cad1@hotmail.com>
2026-02-04 19:12:36 +0000

    Edited comments and added a couple of #evals

--- MA4N1_Platonic_Solids/Current/File6_InequalitySolutionClassification.lean
+++ MA4N1_Platonic_Solids/Current/File6_InequalitySolutionClassification.lean
@@ -3,12 +3,12 @@ import MA4N1_Platonic_Solids.Current.File4_PlatonicGraphDefs
 import MA4N1_Platonic_Solids.Current.File5a_InequalityDerivationTheorem
 
 
--- This file classifies the pairs (m, n) that arise from the definition of PlatonicGraph, proves
--- that there are only 5 possibilities, and then maps each to its associated named Platonic graph.
+-- This file classifies the pairs (m, n) that arise from `PlatonicGraph`, proves that there are
+-- only five possibilities, and then maps each to its associated named Platonic graph.
 
 
 -- We shift the solution set down to avoid subtraction in ℕ.
-lemma shift_classify : {(m, n) : ℕ × ℕ | (m + 1) * (n + 1)< 4}
+lemma shift_classify : {(m, n) : ℕ × ℕ | (m + 1) * (n + 1) < 4}
  = {(0, 0), (0, 1), (0, 2), (1, 0), (2, 0)} := by
   ext x
   aesop
@@ -29,8 +29,8 @@ lemma shift : (· + (3,3)) '' {(m, n) : ℕ × ℕ | (m + 1) * (n + 1) < 4}
   obtain _|_|_|fst := fst <;> try grind
   obtain _|_|_|snd := snd <;> grind
 
--- Defining PlatonicPairs to be the the solution set, avoiding continuously rewriting out the set.
--- They are the 5 possible pairs arising from the definition of a PlatonicGraph.
+-- Defining `PlatonicPairs` to be the solution set, avoiding continuously rewriting out the set.
+-- They are the five possible pairs arising from the inequalities for a `PlatonicGraph`.
 def PlatonicPairs : Set (ℕ × ℕ) := {(3, 3), (3, 4), (3, 5), (4, 3), (5, 3)}
 
 -- Classification theorem for the pairs defined by the inequalities provided.
@@ -44,11 +44,8 @@ theorem classify_mn : ({(m, n) : ℕ × ℕ | m > 2 ∧ n > 2 ∧ (m - 2) * (n -
       aesop
     simpa [PlatonicPairs] using h
 
-
-
--- We now add context to what (m, n) are now. That is, for any PlatonicGraph, its associated pair
--- (Pt.m, Pt.regular.n) lie in the set PlatonicPairs. Pt.m and Pt.regular.n encode the inequalities
--- in the previous classification, so this is a trivial corollary.
+-- Corollary: any `PlatonicGraph` has (Pt.m, Pt.regular.n) in `PlatonicPairs`.
+-- This adds context to (m, n), by setting m = Pt.m and n = Pt.regular.n
 theorem classify_Pt_mn_members (Pt : PlatonicGraph) : (Pt.m, Pt.regular.n) ∈ PlatonicPairs := by
   have hmem :
     (Pt.m, Pt.regular.n) ∈
@@ -57,7 +54,10 @@ theorem classify_Pt_mn_members (Pt : PlatonicGraph) : (Pt.m, Pt.regular.n) ∈ P
   simpa [classify_mn] using hmem
 
 
--- Map a pair (m, n) to its corresponding named PlatonicGraph.
+-- Map a pair (m, n) to its corresponding named `PlatonicGraph`
+-- The names correspond to the classical Platonic solids, justified externally via Steinitz’s
+-- Theorem as explained in the README.md file. This is a conventional labeling, not an additional
+-- formal result.
 def mn_graph_name : (ℕ × ℕ) → String
 | (3, 3) => "Tetrahedron Graph"
 | (3, 4) => "Octahedron Graph"
@@ -66,15 +66,16 @@ def mn_graph_name : (ℕ × ℕ) → String
 | (5, 3) => "Dodecahedron Graph"
 | _      => "[Not a Platonic Graph]"
 
--- This is the set of names obtained from the classified PlatonicPairs. We encode these names in the
--- set string PlatonicGraphNames in order to avoid having to rewrite inside the final theorem, and
--- maintain readability.
+-- This is the set of names obtained from the classified `PlatonicPairs`. We encode these names in
+-- the set PlatonicGraphNames of strings in order to avoid having to rewrite inside the final
+-- theorem, and maintain readability.
 def PlatonicGraphNames : Set String :=
   mn_graph_name '' PlatonicPairs
 
 
 -- This is the final classification, combining the previous theorem and the mapping to a named
--- Platonic Graph. That is, 'Any PlatonicGraph corresponds to one of the five named Platonic Graphs'
+-- Platonic graph. That is, "Any `PlatonicGraph` corresponds to one of the five named Platonic
+-- Graphs"
 theorem classify_Pt_name_mem (Pt : PlatonicGraph) :
   mn_graph_name (Pt.m, Pt.regular.n) ∈ PlatonicGraphNames := by
     have hmem : (Pt.m, Pt.regular.n) ∈ PlatonicPairs :=
@@ -82,3 +83,11 @@ theorem classify_Pt_name_mem (Pt : PlatonicGraph) :
 
     unfold PlatonicGraphNames
     refine ⟨(Pt.m, Pt.regular.n), hmem, rfl⟩
+
+-- We can check that we have in fact mapped each element of `PlatonicPairs` (m, n) to a named
+-- `PlatonicGraph`. Although our formalisation works entirely at the level of graphs
+-- (see README.md), each pair is mapped to its classical Platonic solid graph, and as such the name
+-- reflects this.
+#eval mn_graph_name (3,5)
+#eval mn_graph_name (4,3)
+#eval mn_graph_name (5,6)
